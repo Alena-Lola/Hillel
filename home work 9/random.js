93661c4371f9d6190576022c925fe2a0f14bc032
Alena Savchenko <alena.sav@icloud.com>
2023-01-01 23:28:54 -0500

    first

---
+++ home work 9/random.js
@@ -0,0 +1,7 @@
+function arrayRandElement(arr) {
+    let rand = Math.floor(Math.random() * arr.length);
+    return arr[rand];
+}
+let arr = [1,3,6,8,4,7,6,0,8];
+
+console.log(arrayRandElement(arr))
