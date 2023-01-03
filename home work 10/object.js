fecfe69a36a0778aebbfcbf7d679535ebc7b9134
Alena Savchenko <alena.sav@icloud.com>
2023-01-02 23:11:29 -0500

    third

--- home work 10/object.js
+++ home work 10/object.js
@@ -1,4 +1,45 @@
-function genealogicalTree(arr){
+function genealogicalTree(family){
+
+    // властивість name повинна бути приведена до верхнього регістра
+    for (let i=0; i<family.length; i++) {
+        const parents = family[i];
+        parents.name = parents.name.toUpperCase();
+    }
+
+    // властивість cities повинна бути розбита по комі і перетворена в масив
+    for (let i=0; i<family.length; i++) {
+        const parents = family[i];
+        parents.cities = parents.cities.split(',');
+    }
+
+    // властивість money повинна бути округлена до цілого числа.
+    // Якщо властивість money передано у вигляді стрічки повинно привести її до числового типу,
+    // якщо після приведення до типу значення рівне NaN вивести повідомлення в консоль
+    for (let i=0;i<family.length;i++) {
+        const parents = family[i];
+        parents.money = Number.parseInt(parents.money)
+    }
+
+    // властивість name повинна бути приведена до верхнього регістра
+    for (let i=0; i<family.length; i++) {
+        for (let q = 0; q < family[i].children.length; q++) {
+            const children = family[i].children[q];
+            children.name = children.name.toUpperCase();
+        }
+    }
+
+    // в властивість children додати новий елемент з імям 'Stepan' і згенерувати випадковий вік до 18 років
+    for (let i=0; i<family.length; i++) {
+        const children = family[i].children;
+        const newChildren = {
+            "name" : 'Stepan',
+            "age": Math.floor(Math.random()*19)
+        }
+        children.push(newChildren)
+    }
+
+    return family;
+}
 
 
 const peoples = [
@@ -37,50 +78,4 @@ const peoples = [
 ];
 
 
-// властивість name повинна бути приведена до верхнього регістра
-for (let i=0;i<peoples.length;i++) {
-    const parents = peoples[i];
-    parents.name = parents.name.toUpperCase();
-    console.log(parents.name);
-
-}
-
-
-// властивість cities повинна бути розбита по комі і перетворена в масив
-    for (let i=0;i<peoples.length;i++) {
-        const parents = peoples[i];
-       console.log(parents.cities.split(','));
-
-}
-
-
-// властивість money повинна бути округлена до цілого числа.
-//     Якщо властивість money передано у вигляді стрічки повинно привести її до числового типу, якщо після приведення до типу значення рівне NaN вивести повідомлення в консоль
-for (let i=0;i<peoples.length;i++) {
-    const parents = peoples[i];
-    console.log(Number.parseInt(parents.money));
-}
-
-// властивість name повинна бути приведена до верхнього регістра
-for (let i=0;i<peoples.length;i++) {
-    for (let q = 0; q < peoples[i].children.length; q++) {
-        const children = peoples[i].children[q];
-        children.name = children.name.toUpperCase();
-        console.log(children.name);
-    }
-}
-
-
-// в властивість children додати новий елемент з імям 'Stepan' і згенерувати випадковий вік до 18 років
-for (let i=0;i<peoples.length;i++) {
-    for (let q = 0; q < peoples[i].children.length; q++) {
-        const children = peoples[i].children[q];
-        children.name={}
-        console.log(children.name.name= 'Stepan',Math.floor(Math.random()*19));
-    }
-
-}
-return arr;
-}
-arr=[];
-console.log(genealogicalTree(arr));
+console.log(genealogicalTree(peoples))
